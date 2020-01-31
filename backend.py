76972bcb99bc986cee11b8d5efb025f79b80cd2b
Courtney Jacobs <courtneyj3470@gmail.com>
2020-01-31 12:41:06 -0500

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+
+def insert(title,author,year,isbn):  # Pass the 4 arguments to be entered
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+    
+    
+connect()
+insert() #Every time you pass arguements according to insert func (title,author,year,isbn) you will insert into db
+print(view())
